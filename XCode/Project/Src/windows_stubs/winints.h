07b0a73f0c6da4d03ab607c609adc089ce9cac7b
Commander Coder <commandercoder.2010@gmail.com>
2024-04-03 23:41:53 +0100

    Added Preferences and BCD to the build

--- XCode/Project/Src/windows_stubs/winints.h
+++ XCode/Project/Src/windows_stubs/winints.h
@@ -10,6 +10,7 @@
 
 
 #include <stdint.h>
+#include <string>
 typedef uint32_t DWORD;   // DWORD = unsigned 32 bit value
 typedef uint16_t WORD;    // WORD = unsigned 16 bit value
 typedef uint8_t BYTE;     // BYTE = unsigned 8 bit value
